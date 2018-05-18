58c4e4591ecd73b5eb692533ccefa1663db8909c
James Woodward <jwoodward@financialforce.com>
2018-05-17 21:46:50 -0400

    Can scrape data from even more asp pages.

--- importTasks/aspScraper.js
+++ importTasks/aspScraper.js
@@ -2,36 +2,63 @@ const convertDataTypes = require('./convertDataTypes');
 module.exports.scrape = function($, config, callback) {
 
 	const privates = {
-		"expectedColumns": [{
-			"name": "place",
-			"index": 0,
-			"length": 10
-		}, {
-			"name": "firstName",
-			"index": 10,
-			"length": 22
-		}, {
-			"name": "lastName",
-			"index": 32,
-			"length": 24
-		}, {
-			"name": "sex",
-			"index": 56,
-			"length": 12
-		}, {
-			"name": "age",
-			"index": 68, 
-			"length": 8
-		}, {
-			"name": "time",
-			"index": 76,
-			"length": 8
-		}],
+		"expectedColumns": {
+			"C6": [
+				{
+					"name": "place",
+					"index": 0,
+					"length": 10
+				}, {
+					"name": "firstName",
+					"index": 10,
+					"length": 22
+				}, {
+					"name": "lastName",
+					"index": 32,
+					"length": 24
+				}, {
+					"name": "sex",
+					"index": 56,
+					"length": 12
+				}, {
+					"name": "age",
+					"index": 68, 
+					"length": 8
+				}, {
+					"name": "time",
+					"index": 76,
+					"length": 8
+				}],
+			"C4": [{
+					"name": "place",
+					"index": 0,
+					"length": 11
+				}, {
+					"name": "firstName",
+					"index": 11,
+					"length": 21
+				}, {
+					"name": "lastName",
+					"index": 32,
+					"length": 23
+				}, {
+					"name": "time",
+					"index": 55,
+					"length": 8
+				}]
+		},
 
-		extractRow: (rowString) => {
+		getDataRows: ($, metaDistance) => {
+			$('meta[columns]').remove();
+			var $preTag = $('meta[distance="' + metaDistance + '"]+pre');
+			var data = $preTag.text().split('\n');
+			return data;
+		},
+
+		extractRow: (rowString, columnData) => {
 			var rawData = {};
 
-			privates.expectedColumns.map((column, i) => {
+			columnData.map((column, i) => {
 				var columnName = column.name,
 					data = rowString.substring(column.index, column.index + column.length).trim();
 				rawData[columnName] = convertDataTypes(columnName, data);
@@ -53,15 +80,17 @@ module.exports.scrape = function($, config, callback) {
 		}
 	}
 
-	var data = $('pre').text();
-	var rows = data.split('\n');
+	var rows = privates.getDataRows($, config.metaDistance);
 	var headers = rows.shift();
 	var results = [];
+	var columnData = privates.expectedColumns[config.columnStyle];
 
 	rows.map((rowString, i) => {
-		var rawData = privates.extractRow(rowString);
-		results.push(privates.transformRow(rawData));
+		var rawData = privates.extractRow(rowString, columnData);
+		var transformedData = privates.transformRow(rawData);
+		if (transformedData !== null) {
+			results.push(transformedData);
+		}
 	});
-
 	callback(results);
 };
--- importTasks/scraper.js
+++ importTasks/scraper.js
@@ -4,8 +4,8 @@ const jsonfile = require('jsonfile');
 const htmlScraper = require('./htmlScraper');
 const aspScraper = require('./aspScraper');
 
-//var inputFile = 'data/testSourceHtml.json';
-var inputFile = 'data/testSourceAsp.json';
+var inputFile = 'data/testSourceHtml.json';
+//var inputFile = 'data/testSourceAsp.json';
 //var inputFile = 'data/2018_04.json';
 
 jsonfile.readFile(inputFile, (err, eventsToScrape) => {
@@ -23,7 +23,9 @@ extractEvent = (event) => {
 
 		let config = {
 			url: distancePage.url,
-			pageType: event.pageType
+			pageType: event.pageType,
+			metaDistance: distancePage.metaDistance,
+			columnStyle: event.columnStyle
 		}
 
 		var myTestThingy = scrapeResultsFromUrl(config, (raceResults) => {
@@ -63,63 +65,5 @@ scrapeResultsFromUrl = (config, callback) => {
 
 		scraper.scrape($, config, callback);
 	});
-/*
-
-	if (config.pageType === 'html') {
-		scrapeHtml();
-		
-			var $headers = $('table tr th');
-			var columnHeaders = locateColumns($headers);
-			var $results = $('table tr');
-
-			extractResults($results, columnHeaders, callback);
-		});
-	} else if (config.pageType === 'asp') {
-		request(config.url, function(error, response, body) {
-			$ = cheerio.load(body);
-			var data = $('pre').text();
-			var rows = data.split('\n');
-			console.log('let\'s do it');
-			//console.log(rows);
-			var headers = rows[0];
-
-
-			var place = headers.substring(0, 10).trim();
-			var firstName = headers.substring(10, 32).trim();
-			var lastName = headers.substring(32, 56).trim();
-			var sex = headers.substring(56, 68).trim();
-			var age = headers.substring(68, 76).trim();
-			var time = headers.substring(76).trim();
-
-			console.log(place);
-			console.log(firstName);
-			console.log(lastName);
-			console.log(sex);
-			console.log(age);
-			console.log(time);
-
-			var headers1 = rows[1];
-			console.log(headers1);
-
-			var place1 = headers1.substring(0, 10).trim();
-			var firstName1 = headers1.substring(10, 32).trim();
-			var lastName1 = headers1.substring(32, 56).trim();
-			var sex1 = headers1.substring(56, 68).trim();
-			var age1 = headers1.substring(68, 76).trim();
-			var time1 = headers1.substring(76).trim();
-
-			console.log(place1);
-			console.log(firstName1);
-			console.log(lastName1);
-			console.log(sex1);
-			console.log(age1);
-			console.log(time1);
-
-		});
-	} else {
-		
-	}*/
 };
-
-
 
