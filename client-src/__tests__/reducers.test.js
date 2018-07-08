7f196d2c6e92adf8329d98344b2cb878f2d592a0
James Woodward <jwoodward@financialforce.com>
2018-07-08 16:06:54 -0400

    Tests for table sorting

--- client-src/__tests__/reducers.test.js
+++ client-src/__tests__/reducers.test.js
@@ -1,6 +1,8 @@
 import selectedRace from '../reducers/selectedRace'
 import * as actions from '../actions/actions'
 
+import { sortTable, makeTable } from '../reducers/sortTable'
+
 describe('selectedRace reducer', () => {
 	describe('raceId reducer', () => {
 		it('should start with no selected race', () => {
@@ -57,7 +59,46 @@ describe('selectedRace reducer', () => {
 
 	describe('tableReducer', () => {
 		it('should start with default table', () => {
-			//Need to work out how to do this properly
+			var expectedTable = makeTable();
+			expect(selectedRace(undefined, {}).table).toEqual(expectedTable);
+		});
+
+		it('should handle RECEIVE_SELECTED_RACE_SUCCESS', () => {
+			var state = {raceId: null},
+				action = {
+					type: actions.RECEIVE_SELECTED_RACE_SUCCESS,
+					records: [{name: 'Nate', age: 10}, {name: 'Alice', age: 7}]
+	        	},
+	        	expected = makeTable([{name: 'Nate', age: 10}, {name: 'Alice', age: 7}]);
+
+			expect(selectedRace(state, action).table).toEqual(expected);
+		});
+
+		it('should handle RECEIVE_SELECTED_RESULTS_FAILED', () => {
+			var state = {
+					raceId: null,
+					table: makeTable([{name: 'Nate', age: 10}, {name: 'Alice', age: 7}])
+				},
+				action = {
+					type: actions.RECEIVE_SELECTED_RESULTS_FAILED
+	        	},
+	        	expected = makeTable([]);
+
+			expect(selectedRace(state, action).table).toEqual(expected);
+		});
+
+		it('should handle SINGLE_RACE_TABLE_SORT_CLICKED', () => {
+			var state = {
+					raceId: null,
+					table: makeTable([{name: 'Nate', age: 10}, {name: 'Alice', age: 7}])
+				},
+				action = {
+					type: actions.SINGLE_RACE_TABLE_SORT_CLICKED,
+					column: 'name'
+	        	},
+	        	expected = makeTable([{name: 'Alice', age: 7}, {name: 'Nate', age: 10}]);
+
+			expect(selectedRace(state, action).table.rows).toEqual(expected.rows);
 		});
 	});
 
@@ -74,5 +115,78 @@ describe('selectedRace reducer', () => {
 			expect(selectedRace({showLocations: false}, action).showLocations).toBe(true);
 			expect(selectedRace({showLocations: true}, action).showLocations).toBe(false);
 		});
+	});
+});
+
+describe('sortTable', () => {
+	describe('makeTable method', () => {
+		it('should default sorting', () => {
+			var expectedValue = {
+				rows: [],
+				sortColumn: null,
+				acsending: true
+			};
+			expect(makeTable()).toEqual(expectedValue);
+		});
+
+		it('should put the data in', () => {
+			var rows = [{name: 'Rob'}],
+				sortColumn = 'name',
+				ascending = false,
+				expectedValue = {
+					rows: rows,
+					sortColumn: sortColumn,
+					acsending: ascending
+				};
+			expect(makeTable(rows, sortColumn, ascending)).toEqual(expectedValue);
+		});
+	});
+
+	describe('sortTablee method', () => {
+		var data = [
+			{name: 'Nate', age: 10, favouriteColor: 'lavender'},
+			{name: 'Alice', age: 7, favouriteColor: 'blue'},
+			{name: 'Teag', age: 8, favouriteColor: 'red'}
+		];
+		it('sorts by the indicated colun', () => {
+			var input = makeTable(data),
+				expectedData = [
+					{name: 'Alice', age: 7, favouriteColor: 'blue'},
+					{name: 'Teag', age: 8, favouriteColor: 'red'},
+					{name: 'Nate', age: 10, favouriteColor: 'lavender'}
+				];
+			expect(sortTable(input, 'age').rows).toEqual(expectedData);
+		});
+		it('sorts then reverses', () => {
+			var input = makeTable(data),
+				expectedData1 = [
+					{name: 'Alice', age: 7, favouriteColor: 'blue'},
+					{name: 'Nate', age: 10, favouriteColor: 'lavender'},
+					{name: 'Teag', age: 8, favouriteColor: 'red'}
+				],
+				expectedData2 = [
+					{name: 'Teag', age: 8, favouriteColor: 'red'},
+					{name: 'Nate', age: 10, favouriteColor: 'lavender'},
+					{name: 'Alice', age: 7, favouriteColor: 'blue'}
+				];
+			var sortedState = sortTable(input, 'favouriteColor');
+			expect(sortedState.rows).toEqual(expectedData1);
+			var secondSortedState = sortTable(sortedState, 'favouriteColor')
+			expect(secondSortedState.rows).toEqual(expectedData2);
+		});
+		it('sorts by distance in miles instead of distance', () => {
+			var distanceData = [
+					{name: 'James', distance: 'ultra', distance_miles: 100.5},
+					{name: 'Nathaniel', distance: 'ultra', distance_miles: 50},
+					{name: 'Jim', distance: 'ultra', distance_miles: 100}				
+				],
+				expectedData = [
+					{name: 'Nathaniel', distance: 'ultra', distance_miles: 50},
+					{name: 'Jim', distance: 'ultra', distance_miles: 100},
+					{name: 'James', distance: 'ultra', distance_miles: 100.5}
+				],
+			input = makeTable(distanceData);
+			expect(sortTable(input, 'distance').rows).toEqual(expectedData);
+		})
 	});
 });
