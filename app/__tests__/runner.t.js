7d5d8c6ad15baedfda9e031f735a7de8960b4144
James Woodward <jwoodward@financialforce.com>
2018-07-29 21:06:27 -0400

    rest of the tests for existing runner queries

--- app/__tests__/runner.t.js
+++ app/__tests__/runner.t.js
@@ -51,4 +51,105 @@ describe('runner route', () => {
 			})
 			.end(done);
 	});
+
+	it('can query a raceId', (done) => {
+		var fakeData = [{data: 'fake data2'}],
+			fakeResponse = {rows: fakeData};
+
+		var queryMock = jest.fn();
+		var releaseMock = jest.fn();
+
+		//Mock two things
+		queryMock.mockImplementation((query, callback) => {
+			callback(false, fakeResponse);
+		});
+		pool.connect.mockImplementation((connectFn) => {
+			connectFn(false, {query: queryMock}, releaseMock);
+		});
+
+		request(app)
+			.get('/runner?raceId=70')
+			.expect('Content-Type', /json/)
+			.expect(200, fakeData) // returns the data we returned from the mocked DB
+			.expect(resp => {
+				expect(queryMock.mock.calls.length).toBe(1);
+				var queryParam = queryMock.mock.calls[0][0];
+				var locationOfWhere = queryParam.indexOf('WHERE');
+				expect(locationOfWhere).toBeGreaterThan(10);
+				var whereClause = queryParam.substring(locationOfWhere);
+				expect(whereClause).toMatch(/runner.race_id = '70'/);
+			})
+			.expect(resp => {
+				expect(releaseMock.mock.calls.length).toBe(1);
+			})
+			.end(done);
+	});
+
+	it('can query a raceId and name in that order', (done) => {
+		var fakeData = [{data: 'fake data2'}],
+			fakeResponse = {rows: fakeData};
+
+		var queryMock = jest.fn();
+		var releaseMock = jest.fn();
+
+		//Mock two things
+		queryMock.mockImplementation((query, callback) => {
+			callback(false, fakeResponse);
+		});
+		pool.connect.mockImplementation((connectFn) => {
+			connectFn(false, {query: queryMock}, releaseMock);
+		});
+
+		request(app)
+			.get('/runner?raceId=70&name=Kim')
+			.expect('Content-Type', /json/)
+			.expect(200, fakeData) // returns the data we returned from the mocked DB
+			.expect(resp => {
+				expect(queryMock.mock.calls.length).toBe(1);
+				var queryParam = queryMock.mock.calls[0][0];
+				var locationOfWhere = queryParam.indexOf('WHERE');
+				expect(locationOfWhere).toBeGreaterThan(10);
+				var whereClause = queryParam.substring(locationOfWhere);
+				expect(whereClause).toMatch(/runner.race_id = '70'/);
+				expect(whereClause).toMatch(/runner.name ILIKE '%Kim%'/);
+			})
+			.expect(resp => {
+				expect(releaseMock.mock.calls.length).toBe(1);
+			})
+			.end(done);
+	});
+
+	it('can query a name and raceId in that order', (done) => {
+		var fakeData = [{data: 'fake data2'}],
+			fakeResponse = {rows: fakeData};
+
+		var queryMock = jest.fn();
+		var releaseMock = jest.fn();
+
+		//Mock two things
+		queryMock.mockImplementation((query, callback) => {
+			callback(false, fakeResponse);
+		});
+		pool.connect.mockImplementation((connectFn) => {
+			connectFn(false, {query: queryMock}, releaseMock);
+		});
+
+		request(app)
+			.get('/runner?name=morse&raceId=2')
+			.expect('Content-Type', /json/)
+			.expect(200, fakeData) // returns the data we returned from the mocked DB
+			.expect(resp => {
+				expect(queryMock.mock.calls.length).toBe(1);
+				var queryParam = queryMock.mock.calls[0][0];
+				var locationOfWhere = queryParam.indexOf('WHERE');
+				expect(locationOfWhere).toBeGreaterThan(10);
+				var whereClause = queryParam.substring(locationOfWhere);
+				expect(whereClause).toMatch(/runner.race_id = '2'/);
+				expect(whereClause).toMatch(/runner.name ILIKE '%morse%'/);
+			})
+			.expect(resp => {
+				expect(releaseMock.mock.calls.length).toBe(1);
+			})
+			.end(done);
+	});
 });
